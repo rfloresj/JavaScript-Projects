78e159df00795205367838e79326396ac8a0b330
rflores <104962825+rfloresj@users.noreply.github.com>
2023-09-26 21:50:18 -0600

    getRandomNumber-function

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -5,11 +5,8 @@ const color = document.querySelector('.color');
 btn.addEventListener('click', () => {
     // get random number between 0 - 3
     const randomNumber = getRandomNumber();
-    console.log(randomNumber)
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 })
 
-function getRandomNumber () {
-    return Math.floor(Math.random() * colors.length);
-}
+const getRandomNumber = () => Math.floor(Math.random() * colors.length);
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -6,8 +6,10 @@ const color = document.querySelector('.color')
 btn.addEventListener('click', ()=>{
     let hexColor = '#';
     for (let i = 0; i < 6; i++) {
-        hexColor += hex[0]
+        hexColor += hex[getRandomNumber()]
     }
 color.textContent = hexColor;
 document.body.style.backgroundColor = hexColor;
 })
+
+const getRandomNumber = () => Math.floor(Math.random() * hex.length);
