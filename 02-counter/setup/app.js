ad1d2ad9d96c61802fa214942fd542d69abd9e90
rflores <104962825+rfloresj@users.noreply.github.com>
2023-10-01 10:15:57 -0600

    btn-event-listener-p1

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -5,6 +5,12 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
-console.log(btns);
-
-
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const styles = e.currentTarget.classList;
+    if (styles.contains('decrease')) {
+        count--;
+    }
+    value.textContent = count;
+  });
+});
