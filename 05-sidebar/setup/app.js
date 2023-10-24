5b1928e8f9ca176883ebe34fb63d2d5c193f38f3
rflores <104962825+rfloresj@users.noreply.github.com>
2023-10-24 09:51:14 -0600

    Add-event-show-or-hide-sidebar

---
+++ 05-sidebar/setup/app.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', () => {
+    if(sidebar.classList.contains('show-sidebar')){
+        sidebar.classList.remove('show-sidebar');
+    } else {
+        sidebar.classList.add('show-sidebar')
+    }
+});
