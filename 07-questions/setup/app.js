3843480090da7d05aa3f553a4cf0f31e730b29d6
rflores <104962825+rfloresj@users.noreply.github.com>
2023-10-28 10:32:15 -0600

    using-selectors-inside-the-element

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,11 +1,27 @@
 //using selectors inside the element
-// traversing the dom
+const questions = document.querySelectorAll(".question");
+questions.forEach((question) => {
+  // console.log(question)
+  const btn = question.querySelector(".question-btn");
+  //   console.log(btn);
+  btn.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
 
-const btns = document.querySelectorAll(".question-btn");
-
-btns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const question = e.currentTarget.parentElement.parentElement;
     question.classList.toggle("show-text");
   });
 });
+
+// traversing the dom
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
