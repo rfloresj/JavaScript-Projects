fd02c3bd888826d512ea1e9c7aba919d2ad36d52
rflores <104962825+rfloresj@users.noreply.github.com>
2023-11-01 09:19:30 -0600

    adding-event-DOMContentLoaded-map-method

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -1,3 +1,4 @@
+// items
 const menu = [
   {
     id: 1,
@@ -72,3 +73,13 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+const sectionCenter = document.querySelector(".section-center");
+
+window.addEventListener("DOMContentLoaded", () => {
+  let displayMenu = menu.map((item) => {
+    // console.log(item);
+    return `<h1>${item.title}</h1>`;
+  });
+  console.log(displayMenu);
+});
