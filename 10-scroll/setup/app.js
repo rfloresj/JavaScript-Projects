1838daab66dc969a098bfc3b7309df1410b2754c
rflores <104962825+rfloresj@users.noreply.github.com>
2023-12-05 08:54:09 -0600

    currentTarget-id

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -50,5 +50,13 @@ scrollLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
     // prevent default
     e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    let position = element.offsetTop;
+    window.scrollTo({
+      left: 0,
+      top: 100,
+    });
   });
 });
