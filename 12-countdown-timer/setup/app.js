0f9594718102788b6f20cb1f09f2f58ce5c9898e
rflores <104962825+rfloresj@users.noreply.github.com>
2023-12-17 12:26:30 -0600

    text-content-year-futureDate

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -27,4 +27,7 @@ const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
 let futureDate = new Date(2024, 4, 24, 17, 30, 0);
-console.log(futureDate);
+
+const year = futureDate.getFullYear();
+
+giveaway.textContent = `giveaway ends on ${year}`;
