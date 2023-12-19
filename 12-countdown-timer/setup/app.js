f02b808e51c5e7cb1dd9e5d9c02611eebc59a833
rflores <104962825+rfloresj@users.noreply.github.com>
2023-12-19 11:08:25 -0600

    getMonth-getDay-getDate

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -26,11 +26,17 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2024, 4, 24, 11, 30, 0);
+let futureDate = new Date(2024, 3, 25, 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
 
-giveaway.textContent = `giveaway ends on ${year}
+let month = futureDate.getMonth();
+month = months[month];
+let weekday = weekdays[futureDate.getDay()];
+
+const date = futureDate.getDate();
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year}
 ${hours}:${minutes}am`;
