90d4b64a00ab405ac9ec552dc9a4386739811fef
rflores <104962825+rfloresj@users.noreply.github.com>
2024-01-02 08:42:18 -0600

    random-values

--- 13-lorem-ipsum/setup/app.js
+++ 13-lorem-ipsum/setup/app.js
@@ -19,11 +19,12 @@ const result = document.querySelector(".lorem-text");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const value = parseInt(amount.value);
+  const random = Math.floor(Math.random() * text.length);
 
   //empty
   // -1
   // > 9
   if (isNaN(value) || value < 0 || value > 9) {
-    result.innerHTML = `<p class="result">${text[0]}</p>`;
+    result.innerHTML = `<p class="result">${text[random]}</p>`;
   }
 });
