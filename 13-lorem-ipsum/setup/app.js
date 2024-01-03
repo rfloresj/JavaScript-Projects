08eb81c162ec0ecd1ed86aa42e2765827d34880a
rflores <104962825+rfloresj@users.noreply.github.com>
2024-01-03 11:20:50 -0600

    else-slice-map-methods

--- 13-lorem-ipsum/setup/app.js
+++ 13-lorem-ipsum/setup/app.js
@@ -24,7 +24,11 @@ form.addEventListener("submit", (e) => {
   //empty
   // -1
   // > 9
-  if (isNaN(value) || value < 0 || value > 9) {
+  if (isNaN(value) || value <= 0 || value > 9) {
     result.innerHTML = `<p class="result">${text[random]}</p>`;
+  } else {
+    let tempText = text.slice(0, value);
+    tempText = tempText.map((item) => `<p class="result">${item}</p>`).join("");
+    result.innerHTML = tempText;
   }
 });
