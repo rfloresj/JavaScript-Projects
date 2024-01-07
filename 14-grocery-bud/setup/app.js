d0cbb89956c2d72b8fc63b450fede48cdac33160
rflores <104962825+rfloresj@users.noreply.github.com>
2024-01-07 13:33:09 -0600

    addItem-conditionals

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -19,7 +19,13 @@ function addItem(e) {
   e.preventDefault();
   const value = grocery.value;
   const id = new Date().getTime().toString();
-  console.log(id);
+  if (value !== "" && editFlag === false) {
+    console.log("add item to the list");
+  } else if (value !== "" && editFlag === true) {
+    console.log("editing");
+  } else {
+    console.log("empty value");
+  }
 }
 // ****** LOCAL STORAGE **********
 
