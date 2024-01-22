84399669998b55407b653f34fb7196423a5ad06c
rflores <104962825+rfloresj@users.noreply.github.com>
2024-01-22 10:27:00 -0600

    addToLocalStorage

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -118,6 +118,13 @@ function setBackToDefault() {
 }
 // ****** LOCAL STORAGE **********
 function addToLocalStorage(id, value) {
+  const grocery = { id, value };
+  let items = localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+  console.log(items);
+  items.push(grocery);
+  localStorage.setItem("list", JSON.stringify(items));
   // console.log("added to local storage");
 }
 function removeFromLocalStorage(id) {}
@@ -127,8 +134,8 @@ function editLocalStorage(id, value) {}
 // getItem
 // removeItem
 // save as strings
-localStorage.setItem("orange", JSON.stringify(["item", "item2"]));
-const oranges = JSON.parse(localStorage.getItem("orange"));
-console.log(oranges);
-localStorage.removeItem("orange");
+// localStorage.setItem("orange", JSON.stringify(["item", "item2"]));
+// const oranges = JSON.parse(localStorage.getItem("orange"));
+// console.log(oranges);
+// localStorage.removeItem("orange");
 // ****** SETUP ITEMS **********
